886daefe93c1fdc9b3c2b55b5785fe8855434bdb
Chaitra-bhat <34712573+Chaitra-bhat@users.noreply.github.com>
2021-12-19 19:01:49 +0530

    UART projects

---
+++ BARE_METAL_PROGRAMMING_STM32F446RE/WORKSPACE/08_usart_modular/Src/main.c
@@ -0,0 +1,13 @@
+#include<stm32f4xx.h>
+#include<stdint.h>
+#include<stdio.h>
+#include<uart.h>
+
+int main()
+{
+
+	while(1)
+	{
+		printf("HELLO CHAITRA.....");
+	}
+}
---
+++ BARE_METAL_PROGRAMMING_STM32F446RE/WORKSPACE/09_usart_rx/Debug/Src/subdir.mk
@@ -0,0 +1,35 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/main.c \
+../Src/syscalls.c \
+../Src/sysmem.c \
+../Src/uart.c 
+
+OBJS += \
+./Src/main.o \
+./Src/syscalls.o \
+./Src/sysmem.o \
+./Src/uart.o 
+
+C_DEPS += \
+./Src/main.d \
+./Src/syscalls.d \
+./Src/sysmem.d \
+./Src/uart.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/main.o: ../Src/main.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F4 -DSTM32F446RETx -DNUCLEO_F446RE -DSTM32F446xx -c -I../Inc -I"C:/Users/Navjeevan/OneDrive/Documents/GitHub/STM32/BARE_METAL_PROGRAMMING_STM32F446RE/OTHERS/CMSIS/Include" -I"C:/Users/Navjeevan/OneDrive/Documents/GitHub/STM32/BARE_METAL_PROGRAMMING_STM32F446RE/OTHERS/CMSIS/Device/ST/STM32F4xx/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/main.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Src/syscalls.o: ../Src/syscalls.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F4 -DSTM32F446RETx -DNUCLEO_F446RE -DSTM32F446xx -c -I../Inc -I"C:/Users/Navjeevan/OneDrive/Documents/GitHub/STM32/BARE_METAL_PROGRAMMING_STM32F446RE/OTHERS/CMSIS/Include" -I"C:/Users/Navjeevan/OneDrive/Documents/GitHub/STM32/BARE_METAL_PROGRAMMING_STM32F446RE/OTHERS/CMSIS/Device/ST/STM32F4xx/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/syscalls.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Src/sysmem.o: ../Src/sysmem.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F4 -DSTM32F446RETx -DNUCLEO_F446RE -DSTM32F446xx -c -I../Inc -I"C:/Users/Navjeevan/OneDrive/Documents/GitHub/STM32/BARE_METAL_PROGRAMMING_STM32F446RE/OTHERS/CMSIS/Include" -I"C:/Users/Navjeevan/OneDrive/Documents/GitHub/STM32/BARE_METAL_PROGRAMMING_STM32F446RE/OTHERS/CMSIS/Device/ST/STM32F4xx/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/sysmem.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Src/uart.o: ../Src/uart.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F4 -DSTM32F446RETx -DNUCLEO_F446RE -DSTM32F446xx -c -I../Inc -I"C:/Users/Navjeevan/OneDrive/Documents/GitHub/STM32/BARE_METAL_PROGRAMMING_STM32F446RE/OTHERS/CMSIS/Include" -I"C:/Users/Navjeevan/OneDrive/Documents/GitHub/STM32/BARE_METAL_PROGRAMMING_STM32F446RE/OTHERS/CMSIS/Device/ST/STM32F4xx/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/uart.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
